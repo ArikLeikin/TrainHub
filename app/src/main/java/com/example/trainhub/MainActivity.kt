acd3df83a7074d52c183a3b04082f3ce99039887
Arik <arik.l@hiredscore.com>
2024-05-28 16:31:36 +0300

    fix

--- app/src/main/java/com/example/trainhub/MainActivity.kt
+++ app/src/main/java/com/example/trainhub/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        supportActionBar?.hide()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
