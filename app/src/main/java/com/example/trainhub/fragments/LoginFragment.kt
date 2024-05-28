82c3e685fabd667ab53ba8502a1e23823216d320
Roie Yechilevich <roiy0987@gmail.com>
2024-05-28 15:56:21 +0300

    add some navigation to register

--- app/src/main/java/com/example/trainhub/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/trainhub/fragments/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.trainhub.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +9,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.navigation.Navigation
 import com.example.trainhub.R
 
 
 class LoginFragment : Fragment() {
+    private var emailEditText: EditText? = null
+    private var passwordEditText: EditText? =null
+    private var loginButton: Button? = null
+    private var errorLoginTextView: TextView? = null
+    private var signUpTextView:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,41 +31,42 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_login, container, false)
-        //setUpUI(view)
+        setUpUI(view)
         return view
     }
 
     private fun setUpUI(view: View) {
-        val emailEditText: EditText = view.findViewById(R.id.etLoginEmail)
-        val passwordEditText: EditText = view.findViewById(R.id.etLoginPassword)
-        val loginButton: Button = view.findViewById(R.id.btnLogin)
-        val errorLoginTextView: TextView = view.findViewById(R.id.tvLoginError)
+        emailEditText = view.findViewById(R.id.etLoginEmail)
+        passwordEditText = view.findViewById(R.id.etLoginPassword)
+        loginButton = view.findViewById(R.id.btnLogin)
+        errorLoginTextView = view.findViewById(R.id.tvLoginError)
+        signUpTextView = view.findViewById(R.id.tvLoginSignUp)
+        val action = Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_signUpFragment)
+        signUpTextView?.setOnClickListener(action)
 
-        loginButton.setOnClickListener{
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+        loginButton?.setOnClickListener{
+            val email = emailEditText?.text.toString()
+            val password = passwordEditText?.text.toString()
             if(validate(email,password,errorLoginTextView)){
                 //TODO check if email and password are in DB
             }
         }
     }
 
-    private fun validate(email: String, password: String,error:TextView): Boolean {
+    private fun validate(email: String, password: String,error:TextView?): Boolean {
         if (!email.contains("@")) {
-            error.text = "Email must contain @"
+            error?.text = "Email must contain @"
             return false
         }
 
         if (password.length <= 6) {
-            error.text = "Password must be more than 6 characters"
+            error?.text = "Password must be more than 6 characters"
             return false
         }
         return true
     }
 
-    fun signUp(view: View) {
-        //TODO change to register fragment
-    }
+
 
 
 }
