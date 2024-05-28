86435f2951807022bd997c08c55005c0327e2dd6
Arik <arik.l@hiredscore.com>
2024-05-28 16:41:43 +0300

    changes to class structure: init + validation

--- app/src/main/java/com/example/trainhub/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/trainhub/fragments/SignUpFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.Navigation
 import com.example.trainhub.R
 
@@ -14,6 +15,7 @@ class SignUpFragment : Fragment() {
 
     private var emailEditText: EditText? = null
     private var passwordEditText: EditText? = null
+    private var errorLoginTextView: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,21 +25,34 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    private fun setUpUi(view: View){
         // Initialize EditText fields
         emailEditText = view.findViewById(R.id.etRegisterEmail)
         passwordEditText = view.findViewById(R.id.etRegisterPassword)
+        errorLoginTextView = view.findViewById(R.id.tvRegisterError)
 
         view.findViewById<Button>(R.id.btnRegister).setOnClickListener {
-            // Handle login button click
-            // Example: validate inputs and perform login/signup action
             val email = emailEditText?.text.toString()
             val password = passwordEditText?.text.toString()
             // Your login/signup logic here
+            if(validate(email,password,errorLoginTextView)){
+                //TODO check if email and password are in DB
+            }
             Navigation.findNavController(view).navigate(R.id.loginFragment)
         }
-
     }
+
+    private fun validate(email: String, password: String,error: TextView?): Boolean {
+        if (!email.contains("@")) {
+            error?.text = "Email must contain @"
+            return false
+        }
+
+        if (password.length <= 6) {
+            error?.text = "Password must be more than 6 characters"
+            return false
+        }
+        return true
+    }
+
 }
