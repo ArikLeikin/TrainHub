a63537e4104d4e252aa18fa10ae92d27b2f1e16b
Roie Yechilevich <roiy0987@gmail.com>
2024-05-29 13:36:04 +0300

    added dao for post and user

--- app/src/main/java/com/example/trainhub/models/dao/PostDao.kt
+++ app/src/main/java/com/example/trainhub/models/dao/PostDao.kt
@@ -1,4 +1,36 @@
 package com.example.trainhub.models.dao
 
-class PostDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.example.trainhub.models.entities.Post
+import com.example.trainhub.models.entities.User
+
+
+@Dao
+interface PostDao {
+
+    @Query("SELECT * FROM posts")
+    fun getAllPosts(): List<Post>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(post: Post)
+
+    @Transaction
+    @Query("SELECT * FROM posts where id = :id") //Note: id is user id or post id
+    fun getPostByID(id: String): Post
+
+    @Transaction
+    @Query("SELECT * FROM posts where userId = :userId")
+    fun getPostsByUserId(userId: String): List<Post>
+
+    @Update
+    fun updatePost(post: Post)
+
+    @Delete
+    fun deletePost(post: Post)
 }
--- app/src/main/java/com/example/trainhub/models/entities/Post.kt
+++ app/src/main/java/com/example/trainhub/models/entities/Post.kt
@@ -2,10 +2,18 @@ package com.example.trainhub.models.entities
 
 import androidx.room.PrimaryKey
 import androidx.room.Entity
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.FieldValue
 
-@Entity
-data class Post(@PrimaryKey var id:String){
+@Entity(tableName = "posts")
+data class Post(@PrimaryKey var id:String,
+                var title: String,
+                var description: String,
+                var imageUrl: String,
+                var userId: String,
+                var workoutCategory: String,
+                var location: String,
+                var lastUpdated: Long?=null){
+
         //TODO POST entity
+
+
 }
