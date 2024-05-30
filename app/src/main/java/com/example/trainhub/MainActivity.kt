bf1914f7ac2ddffc6bef854206d40add29f5eceb
Dolev <dolevs1890@gmail.com>
2024-05-31 01:19:58 +0300

    Adding bottom navigation

--- app/src/main/java/com/example/trainhub/MainActivity.kt
+++ app/src/main/java/com/example/trainhub/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.trainhub
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.navHostMain) as? NavHostFragment
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView?.let {
+            Navigation.setViewNavController(it, navController)
+        }
+        navController = navHostFragment?.navController
+        navController?.addOnDestinationChangedListener { _, destination, _  ->
+            when (destination.id) {
+                R.id.loginFragment, R.id.signUpFragment -> bottomNavigationView.visibility = View.GONE
+                else -> bottomNavigationView.visibility = View.VISIBLE
+
+            }
+        }
+
+
 
     }
+
+
 }
 
