52349c228bbf890f1c3111439c324b4adf0da63c
Arik <arik.l@hiredscore.com>
2024-06-01 20:14:13 +0300

    added changes to signup page

--- app/src/main/java/com/example/trainhub/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/trainhub/fragments/SignUpFragment.kt
@@ -1,56 +1,111 @@
 package com.example.trainhub.fragments
 
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.trainhub.R
 
 class SignUpFragment : Fragment() {
-
+    private var nicknameEditText: EditText? = null
     private var emailEditText: EditText? = null
     private var passwordEditText: EditText? = null
-    private var errorLoginTextView: TextView? = null
     private var registerButton: Button? = null
+    private var profileImgView: ImageView? = null
+    private var backToLoginText: TextView? = null
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
+    private var selectedImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        setUpUi(view)
+
+        return view
     }
 
     private fun setUpUi(view: View){
-        // Initialize EditText fields
+        nicknameEditText = view.findViewById(R.id.etNickname)
+        profileImgView = view.findViewById(R.id.ivProfileImage)
+        profileImgView?.setOnClickListener {
+            openImagePicker()
+            Toast.makeText(context,"Choose an Image", Toast.LENGTH_SHORT).show()
+            Log.i(TAG,"change profileImageButton clicked")
+        }
         emailEditText = view.findViewById(R.id.etRegisterEmail)
         passwordEditText = view.findViewById(R.id.etRegisterPassword)
-        errorLoginTextView = view.findViewById(R.id.tvRegisterError)
         registerButton = view.findViewById(R.id.btnRegister)
         registerButton?.setOnClickListener {
+            val nickname = nicknameEditText?.text.toString()
             val email = emailEditText?.text.toString()
             val password = passwordEditText?.text.toString()
-            // Your login/signup logic here
-            if(validate(email,password,errorLoginTextView)){
-                //TODO check if email and password are in DB
+            if(validate(nickname,email,password)){
+                Navigation.findNavController(view).navigate(R.id.loginFragment)
             }
-            Navigation.findNavController(view).navigate(R.id.loginFragment)
+        }
+        backToLoginText = view.findViewById(R.id.tvBackToLogin)
+        backToLoginText?.setOnClickListener{
+            findNavController().navigateUp()
+        }
+
+        selectedImageUri = Uri.parse("android.resource://${requireActivity().packageName}/${R.drawable.user_profile}")
+        Glide.with(this)
+            .load(selectedImageUri)
+            .into(profileImgView!!)
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){loadedProfileImageInPickerHandler(it)}
+    }
+
+    private fun loadedProfileImageInPickerHandler(result: ActivityResult){
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            selectedImageUri = data?.data
+            if (selectedImageUri != null) {
+                Glide.with(this)
+                    .load(selectedImageUri)
+                    .into(profileImgView!!)
+            }
         }
     }
 
-    private fun validate(email: String, password: String,error: TextView?): Boolean {
-        if (!email.contains("@")) {
-            error?.text = "Email must contain @"
+    private fun openImagePicker() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        imagePickerLauncher.launch(intent)
+    }
+
+    private fun validate(nickname: String, email: String, password: String): Boolean {
+        if (nickname.length < 3){
+            Toast.makeText(context,"Nickname must contain at least 3 characters", Toast.LENGTH_SHORT).show()
             return false
         }
 
-        if (password.length <= 6) {
-            error?.text = "Password must be more than 6 characters"
+        if (!email.contains("@")) {
+            Toast.makeText(context,"Email must contain @", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (password.length < 6) {
+            Toast.makeText(context,"Password must contain at least 6 characters", Toast.LENGTH_SHORT).show()
             return false
         }
         return true
