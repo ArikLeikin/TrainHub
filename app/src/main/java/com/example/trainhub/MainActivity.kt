e8badd84ce9aadfe1a25c0a606b336e869d3806c
Roie Yechilevich <roiy0987@gmail.com>
2024-06-04 11:39:43 +0300

    Added on progressBar to AddPostFragment

--- app/src/main/java/com/example/trainhub/MainActivity.kt
+++ app/src/main/java/com/example/trainhub/MainActivity.kt
@@ -17,44 +17,50 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null
+    private val homeFragment = HomeFragment()
+    private var bottomNavigationView:BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.navHostMain) as? NavHostFragment
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView?.let {
             Navigation.setViewNavController(it, navController)
         }
 
 
-        val homeFragment = HomeFragment()
         val discoverFragment = DiscoverFragment()
-        val addPostFragment = AddPostFragment()
         val locationFragment = LocationFragment()
         val profileFragment = ProfileFragment()
-        bottomNavigationView.setOnItemSelectedListener {
+
+        bottomNavigationView?.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.icHome -> replaceFragment(homeFragment)
                 R.id.icDiscover -> replaceFragment(discoverFragment)
-                R.id.icAddPost -> replaceFragment(addPostFragment)
+                R.id.icAddPost -> replaceFragment(AddPostFragment())
                 R.id.icLocation -> replaceFragment(locationFragment)
                 R.id.icProfile -> replaceFragment(profileFragment)
             }
             true
         }
 
+
         navController = navHostFragment?.navController
         navController?.addOnDestinationChangedListener { _, destination, _  ->
             when (destination.id) {
-                R.id.loginFragment, R.id.signUpFragment -> bottomNavigationView.visibility = View.GONE
-                else -> bottomNavigationView.visibility = View.VISIBLE
+                R.id.loginFragment, R.id.signUpFragment -> bottomNavigationView?.visibility = View.GONE
+                else -> bottomNavigationView?.visibility = View.VISIBLE
 
             }
         }
     }
+    fun moveToHome(){
+        bottomNavigationView?.selectedItemId = R.id.icHome
+        replaceFragment(homeFragment)
+    }
 
-    private fun replaceFragment(fragment: Fragment){
+    fun replaceFragment(fragment: Fragment){
         if(fragment!=null){
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.navHostMain,fragment)
