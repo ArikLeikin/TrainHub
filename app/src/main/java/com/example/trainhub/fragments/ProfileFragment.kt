8d4899b1775e46e7f84e6425bf44b962ea80128b
Dolev <dolevs1890@gmail.com>
2024-06-20 19:04:41 +0300

    adding dialog for edit name

--- app/src/main/java/com/example/trainhub/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/trainhub/fragments/ProfileFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.navigation.NavDirections
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.trainhub.MainActivity
+import com.bumptech.glide.Glide
 import com.example.trainhub.R
 import com.example.trainhub.TrainHubApplication
 import com.example.trainhub.adapters.PostRecyclerAdapter
@@ -22,21 +25,83 @@ class ProfileFragment : Fragment() {
 
     private lateinit var recyclerViewPosts: RecyclerView
     private lateinit var postsAdapter: PostRecyclerAdapter
-    private var editProfile: TextView? = null
     private var postsList: List<PostWithUser> = listOf()
     private val profileViewModel: ProfileViewModel = ProfileViewModel()
+    private var name: TextView? = null
+    private var email:TextView? = null
+    private var profilePic:ImageView? = null
+    private var btnEdit : Button? = null
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+            name = view.findViewById(R.id.etName)
+            email = view.findViewById(R.id.tvEmail)
+            profilePic = view.findViewById(R.id.ivProfile)
+            btnEdit = view.findViewById(R.id.btnEdit)
+
+
+        val imageUrl = TrainHubApplication.Globals.currentUser?.profileImageUrl
+        Log.d("ProfileFragment", "onCreateView: $imageUrl")
+            email?.text = TrainHubApplication.Globals.currentUser?.email
+             name?.text = TrainHubApplication.Globals.currentUser?.name
+
+
+        if (imageUrl != null && imageUrl.startsWith("http")) {
+            Glide.with(this).load(imageUrl).into(profilePic!!)
+        } else {
+            Glide.with(this).load(R.drawable.user_profile).into(profilePic!!)
+        }
+
+        btnEdit?.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.dialog_update_name, null)
+            val pb: ProgressBar? = null
+            val oldN = view.findViewById<EditText>(R.id.old_name)
+            val newN = view.findViewById<EditText>(R.id.new_name)
+            val builder = android.app.AlertDialog.Builder(context)
+            oldN.setText(name?.text)
+            val currentName = oldN.text.toString()
+            oldN.isEnabled = false
+            builder.setView(view)
+            val dialog = builder.create()
+            dialog.show()
+            view.findViewById<Button>(R.id.update_name_btn).setOnClickListener {
+                pb?.visibility = View.VISIBLE
+                val newName = newN.text.toString()
+                if(newName.isEmpty()){
+                    newN.error = "Name cannot be empty"
+                    return@setOnClickListener
+                }
+                if(newName == currentName){
+                    newN.error = "Name cannot be the same"
+                    return@setOnClickListener
+                }
+                if (currentName == TrainHubApplication.Globals.currentUser?.name) {
+                    val user = TrainHubApplication.Globals.currentUser
+                    user?.name = newName
+                    profileViewModel.updateProfile(user!!) { isUpdated ->
+                        if (isUpdated) {
+                            pb?.visibility = View.GONE
+                            name?.text = newName
+                            Toast.makeText(context, "Name updated successfully", Toast.LENGTH_SHORT).show()
+                            dialog.dismiss()
+                        }
+                    }
+                }
+            }
+        }
+
+
+
         recyclerViewPosts = view.findViewById(R.id.rvMyPosts)
         recyclerViewPosts.layoutManager = LinearLayoutManager(context)
         postsAdapter = PostRecyclerAdapter(postsList,true)
         recyclerViewPosts.adapter = postsAdapter
-
         profileViewModel.fetchPosts()
         profileViewModel.posts.observe(viewLifecycleOwner) {posts->
             val userPosts = posts.filter { it.post.userId == TrainHubApplication.Globals.currentUser?.id }
@@ -45,12 +110,7 @@ class ProfileFragment : Fragment() {
             postsAdapter.notifyDataSetChanged()
         }
 
-        editProfile = view.findViewById(R.id.edit_profile)
-        editProfile?.setOnClickListener {
-            val activity = context as MainActivity
-            activity.moveToEditProfile()
 
-        }
 
         return view
 
--- app/src/main/java/com/example/trainhub/viewModel/ProfileViewModel.kt
+++ app/src/main/java/com/example/trainhub/viewModel/ProfileViewModel.kt
@@ -1,17 +1,21 @@
 package com.example.trainhub.viewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.trainhub.models.PostModel
 import com.example.trainhub.models.UserModel
+import com.example.trainhub.models.entities.User
 
 class ProfileViewModel: ViewModel() {
     private val um: UserModel = UserModel.instance
     private val pm: PostModel = PostModel.instance
 
     private val _posts = MutableLiveData<List<PostWithUser>>()
+    private val _user = MutableLiveData<User>()
     val posts: LiveData<List<PostWithUser>> get() = _posts
+    val user: LiveData<User> get() = _user
 
     fun fetchPosts() {
         pm.getAllPosts { posts->
@@ -23,6 +27,25 @@ class ProfileViewModel: ViewModel() {
                 }
             }
         }
+
+    }
+
+    fun updateProfile(user: User, callback: (Boolean) -> Unit){
+       um.updateUser(user){isUpdated->
+           if(isUpdated) {
+               UserModel.instance.updateUser(user) { isLocalUpdateSuccessful ->
+                   if (isLocalUpdateSuccessful) {
+                       _user.value = user
+                   }
+               }
+               callback(isUpdated)
+           }
+           else{
+                callback(false)
+           }
+        }
+
+
 
     }
 
